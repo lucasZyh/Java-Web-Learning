6ab1d3166112ed7dc38348ce058fb869ea84540a
lucasZyh <1425724354@qq.com>
2022-12-19 19:56:09 +0800

    学习Request&&Response

---
+++ Chapter10-request-demo/src/main/java/com/itheima/web/request/RequestDemo6.java
@@ -0,0 +1,25 @@
+package com.itheima.web.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet("/req6")
+public class RequestDemo6 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("demo6...");
+        //获取数据
+        Object msg = request.getAttribute("msg");
+        System.out.println(msg);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
